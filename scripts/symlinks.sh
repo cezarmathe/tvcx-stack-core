dbdcd689f543f344b6b145562e54661e8d266c22
cezarmathe <me@cezarmathe.com>
2020-10-08 06:28:13 +0300

    Add symlinks redo and undo

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -6,7 +6,7 @@ function main() {
     local cmd
     cmd="$1"; shift
     if [[ -z "${cmd}" ]]; then
-        printf "%s\n" "Available commands: do undo"
+        printf "%s\n" "Available commands: do redo undo"
         exit 1
     fi
 
@@ -15,12 +15,22 @@ function main() {
         stow --dir "$(pwd)/docker" --target "/srv/docker" .
         stow --dir "$(pwd)/etc" --target "/etc" .
         stow --dir "$(pwd)/ssl" --target "/srv/ssl" .
-        stow --dir "$(pwd)/ssl" --target "/srv/ufw" .
+        stow --dir "$(pwd)/ufw" --target "/srv/ufw" .
+        ;;
+    "redo")
+        stow --dir "$(pwd)/docker" --target "/srv/docker" --restow .
+        stow --dir "$(pwd)/etc" --target "/etc" --restow .
+        stow --dir "$(pwd)/ssl" --target "/srv/ssl" --restow .
+        stow --dir "$(pwd)/ufw" --target "/srv/ufw" --restow .
         ;;
     "undo")
+        stow --dir "$(pwd)/docker" --target "/srv/docker" --delete .
+        stow --dir "$(pwd)/etc" --target "/etc" --delete .
+        stow --dir "$(pwd)/ssl" --target "/srv/ssl" --delete .
+        stow --dir "$(pwd)/ufw" --target "/srv/ufw" --delete .
         ;;
     *)
-        printf "%s\n" "Available commands: do undo"
+        printf "%s\n" "Available commands: do redo undo"
         exit 1
         ;;
     esac

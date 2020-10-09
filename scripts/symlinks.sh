1e067fd1c7f489f25ac144d11f9f76ab94d5d829
cezarmathe <me@cezarmathe.com>
2020-10-09 06:11:35 +0300

    Refactor ssl cert hardlinking

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -14,22 +14,33 @@ function main() {
     "do")
         stow --dir "$(pwd)/docker" --target "/srv/docker" .
         stow --dir "$(pwd)/etc" --target "/etc" .
-        stow --dir "$(pwd)/ssl" --target "/srv/ssl" .
         stow --dir "$(pwd)/ufw" --target "/srv/ufw" .
+        for dir in $(ls $(pwd)/ssl); do
+            mkdir -p "/srv/ssl/${dir}"
+            for file in $(ls $(pwd)/ssl/${dir}/cert); do
+                ln "$(pwd)/ssl/${dir}/cert/${file}" "/srv/ssl/${dir}/${file}"
+            done
+        done
         printf "%s\n" "Done!"
         ;;
     "redo")
         stow --dir "$(pwd)/docker" --target "/srv/docker" --restow .
         stow --dir "$(pwd)/etc" --target "/etc" --restow .
-        stow --dir "$(pwd)/ssl" --target "/srv/ssl" --restow .
         stow --dir "$(pwd)/ufw" --target "/srv/ufw" --restow .
+        rm -rf /srv/ssl/*
+        for dir in $(ls $(pwd)/ssl); do
+            mkdir -p "/srv/ssl/${dir}"
+            for file in $(ls $(pwd)/ssl/${dir}/cert); do
+                ln "$(pwd)/ssl/${dir}/cert/${file}" "/srv/ssl/${dir}/${file}"
+            done
+        done
         printf "%s\n" "Done!"
         ;;
     "undo")
         stow --dir "$(pwd)/docker" --target "/srv/docker" --delete .
         stow --dir "$(pwd)/etc" --target "/etc" --delete .
-        stow --dir "$(pwd)/ssl" --target "/srv/ssl" --delete .
         stow --dir "$(pwd)/ufw" --target "/srv/ufw" --delete .
+        rm -rf /srv/ssl/*
         printf "%s\n" "Done!"
         ;;
     *)

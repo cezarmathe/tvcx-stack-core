49bf340c5e8f2f32416e904cc6c011b53258623d
cezarmathe <me@cezarmathe.com>
2020-10-09 06:15:35 +0300

    Remove hardlink function from scripts/certificate.sh

--- scripts/certificates.sh
+++ scripts/certificates.sh
@@ -2,19 +2,6 @@
 
 # This script will handle certificate requesting and hardlinking for all services.
 
-function hardlink() {
-    for dir in $(ls /srv/ssl); do
-        printf "%s\n" "Hardlinking certificate for ${dir}"
-        cd "/srv/ssl/${dir}"
-        local hardlink_to
-        hardlink_to="$(cat $(pwd)/srv_src.conf | grep hardlink_to | tr -s ' ' | cut -d ' ' -f 3)"
-        ln "$(pwd)/cert/cert.pem" "${hardlink_to}/cert.pem"
-        ln "$(pwd)/cert/cert-key.pem" "${hardlink_to}/cert-key.pem"
-    done
-    printf "%s\n" "Done!"
-
-}
-
 function request() {
     for dir in $(ls $(pwd)/ssl); do
         printf "%s\n" "Requesting certificate for ${dir}"
@@ -30,7 +17,7 @@ function main() {
     local cmd
     cmd="$1"; shift
     if [[ -z "${cmd}" ]]; then
-        printf "%s\n" "Available commands: hardlink request"
+        printf "%s\n" "Available commands: request"
         exit 1
     fi
 
@@ -42,7 +29,7 @@ function main() {
         request
         ;;
     *)
-        printf "%s\n" "Available commands: hardlink request"
+        printf "%s\n" "Available commands: request"
         ;;
     esac
 }

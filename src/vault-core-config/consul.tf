e79de44402c2e1bc605427de40c8da8dc416c77a
cezarmathe <me@cezarmathe.com>
2020-11-03 05:56:29 +0200

    Add vault config for the new consul policies

--- src/vault-core-config/consul.tf
+++ src/vault-core-config/consul.tf
@@ -30,3 +30,51 @@ resource "vault_generic_endpoint" "consul_role_operator" {
 }
 EOT
 }
+
+resource "vault_generic_endpoint" "consul_role_stack_state" {
+  depends_on           = [vault_consul_secret_backend.consul]
+  path                 = "consul/roles/stack_state"
+  ignore_absent_fields = true
+
+  data_json = <<EOT
+{
+  "policies": ["kv_write_stack_state"]
+}
+EOT
+}
+
+resource "vault_generic_endpoint" "consul_role_write_stack_config" {
+  depends_on           = [vault_consul_secret_backend.consul]
+  path                 = "consul/roles/write_stack_config"
+  ignore_absent_fields = true
+
+  data_json = <<EOT
+{
+  "policies": ["kv_write_stack_config"]
+}
+EOT
+}
+
+resource "vault_generic_endpoint" "consul_role_rw_stack" {
+  depends_on           = [vault_consul_secret_backend.consul]
+  path                 = "consul/roles/rw_stack"
+  ignore_absent_fields = true
+
+  data_json = <<EOT
+{
+  "policies": ["kv_write_stack_config", "kw_write_stack_state"]
+}
+EOT
+}
+
+resource "vault_generic_endpoint" "consul_role_read_stack_config_nomad" {
+  depends_on           = [vault_consul_secret_backend.consul]
+  path                 = "consul/roles/read_stack_config_nomad"
+  ignore_absent_fields = true
+
+  data_json = <<EOT
+{
+  "policies": ["kv_read_stack_config_nomad"]
+}
+EOT
+}
